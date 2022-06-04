51804b0b3c5eb7240afb568a7cde6617db917f77
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-05 02:46:03 +0530

    Create LCA_in_a_BST.cpp

---
+++ LCA_in_a_BST.cpp
@@ -0,0 +1,40 @@
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	// Write your code here
+    
+    if(root->data < P->data && root->data < Q->data)
+    {
+        return root->right = LCAinaBST(root->right, P,Q);
+    }
+    if(root->data > P->data && root->data > Q->data)
+    {
+        return root->left = LCAinaBST(root->left,P,Q);
+    }
+    return root;
+}
