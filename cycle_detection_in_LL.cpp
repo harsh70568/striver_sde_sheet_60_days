438d553805ea7db74affeecf7eddcbcfb5e699fb
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-05 02:04:23 +0530

    Create cycle_detection_in_LL.cpp

---
+++ cycle_detection_in_LL.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+
+bool detectCycle(Node *head)
+{
+	//	Write your code here
+    Node* slow = head;
+    Node* fast = head;
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
