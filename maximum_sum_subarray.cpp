f931691b495e4c3cde051f58cbd4167def033a02
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-04 21:44:02 +0530

    Update maximum_sum_subarray.cpp

--- maximum_sum_subarray.cpp
+++ maximum_sum_subarray.cpp
@@ -1,1 +1,20 @@
 
+long long maxSubarraySum(int arr[], int n)
+{
+    
+    long long curr_sum = 0;
+    long long max_sum = 0;    
+    for(int i = 0; i < n; i++)
+    {
+        curr_sum += arr[i];   // adding everytime the value of current element.
+        if(curr_sum > max_sum)    // if at any point my current sum get max then my maximum sum, i will update my maximum sum 
+        {
+            max_sum = curr_sum;
+        }
+        if(curr_sum < 0)   // if at any point my current sum becomes negative i will not intrested in taking subarray till, so gives current sum a fresh start.
+        {
+            curr_sum = 0;
+        }
+    }
+    return max_sum;
+}
