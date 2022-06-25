7bfb19ebd689d13359eeaa75c2f85ffad4e0d146
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-25 16:12:06 +0530

    Create 0_1_knapsack.cpp

---
+++ 0_1_knapsack.cpp
@@ -0,0 +1,31 @@
+int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
+{
+	// Write your code here
+    vector<vector<int>> dp(n+1,vector<int>(w+1,-1));
+    for(int i = 0; i < n+1; i++)
+    {
+        for(int j = 0; j < w+1; j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i = 1; i < n+1; i++)
+    {
+        for(int j = 1; j < w+1; j++)
+        {
+            if(weights[i-1] <= j)
+            {
+                dp[i][j] = max(values[i-1]+dp[i-1][j-weights[i-1]],dp[i-1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][w];
+}
